55152943d453caf0a093edc50cf3dde909dc2d9c
ykc <ykilicarslan@sabanciuniv.edu>
2022-12-22 20:50:26 +0300

    clear

--- MobileApp/wallet/build/macos/Build/Intermediates.noindex/Pods.build/Debug/FMDB.build/Objects-normal/arm64/FMDB_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/build/macos/Build/Intermediates.noindex/Pods.build/Debug/FMDB.build/DerivedSources/FMDB_vers.c
--- MobileApp/wallet/build/macos/Build/Intermediates.noindex/Pods.build/Debug/FMDB.build/Objects-normal/arm64/FMResultSet.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/macos/Pods/FMDB/src/fmdb/FMResultSet.m \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/build/macos/Build/Intermediates.noindex/Pods.build/Debug/FMDB.build/module.modulemap \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/macos/Pods/Target\ Support\ Files/FMDB/FMDB-prefix.pch \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/macos/Pods/FMDB/src/fmdb/FMResultSet.h \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/macos/Pods/FMDB/src/fmdb/FMDatabase.h \
-  /Users/ykc/Documents/Worksplace/Fall2022/ENS491/SUPayment/MobileApp/wallet/macos/Pods/FMDB/src/fmdb/FMDatabasePool.h
